42c4ad16dd30ddbdb79e1ccd012efcf00078781f
oonoren <dayelian9@gmail.com>
2025-09-23 04:44:49 +0900

    Update CORS settings for frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,16 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
-    resource "/api/*", headers: :any, methods: %i[get post patch put delete options]
+    allowed = [
+      %r{\Ahttps://.*\.vercel\.app\z},   # Vercel本番
+      ENV["FRONTEND_ORIGIN"],            # 決まった本番URL
+      "http://localhost:5173",
+      "http://127.0.0.1:5173"
+    ].compact
+
+    origins(*allowed)
+
+    resource "/api/*",
+             headers: :any,
+             methods: %i[get post patch put delete options]
   end
 end

c8534f548388a5134f3492bffc3ae0c70c526813
oonoren <dayelian9@gmail.com>
2025-09-23 02:51:27 +0900

    update config files

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,14 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "http://127.0.0.1:5173"
-    resource "/api/*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
+    # 本番: *.vercel.app と、個別指定 FRONTEND_ORIGIN
+    extra = ENV.fetch("FRONTEND_ORIGIN", nil)
+
+    origins vercel_regex,
+            extra,
+            "http://localhost:5173", "http://127.0.0.1:5173"  # 開発用
+
+    resource "/api/*",
+             headers: :any,
+             methods: %i[get post patch put delete options]
   end
 end

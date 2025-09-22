459786b536e7857fa9d3799ba907fbb1ce47d699
oonoren <dayelian9@gmail.com>
2025-09-23 03:21:41 +0900

    fix(cors): define allowed origins properly

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,12 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # 本番: *.vercel.app と、個別指定 FRONTEND_ORIGIN
     extra = ENV.fetch("FRONTEND_ORIGIN", nil)
 
-    origins vercel_regex,
-            extra,
-            "http://localhost:5173", "http://127.0.0.1:5173"  # 開発用
-
-    resource "/api/*",
-             headers: :any,
-             methods: %i[get post patch put delete options]
+    origins %r{\Ahttps://.*\.vercel\.app\z}, ENV["FRONTEND_ORIGIN"],
+            "http://localhost:5173", "http://127.0.0.1:5173"
+    resource "/api/*", headers: :any, methods: %i[get post patch put delete options]
   end
 end

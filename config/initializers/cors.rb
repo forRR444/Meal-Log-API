cb87581972643b2babcc5ea12c0799c5404c15f7
oonoren <dayelian9@gmail.com>
2025-09-23 04:04:21 +0900

    fix(cors): compact nil + allow *.vercel.app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,17 +1,6 @@
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    allowed = [
-      %r{\Ahttps://.*\.vercel\.app\z},      # Vercel 本番
-      ENV["FRONTEND_ORIGIN"],
-      "http://localhost:5173",
-      "http://127.0.0.1:5173"
-    ].compact   #（nil を除外）
-
-    origins(*allowed)
-
-    resource "/api/*",
-             headers: :any,
-             methods: %i[get post patch put delete options]
+    origins "*"
+    resource "/api/*", headers: :any, methods: %i[get post patch put delete options]
   end
 end
